66fec2f3f8048c863baa2c85ac0181e536e27d38
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-10-31 15:39:52 -0400

    project set up with initial folder structure

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,12 @@
+// import he gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+  type Query {
+    helloWorld: String
+  }
+`;
+
+// export the typeDefs
+module.export = typeDefs;
