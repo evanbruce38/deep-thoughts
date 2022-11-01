859705a3c0ba5e562cba92dd0666e445bb1eedb1
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-10-31 23:23:37 -0400

    added new authentication error message

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,22 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express')
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+      const userData = await User.findOne({})
+
+      .select('-__v -password')
+      .populate('thoughts')
+      .populate('friends');
+
+      return userData;
+    }
+
+    throw new AuthenticationError('Not logegd in');
+  },
     users: async () => {
       return User.find()
         .select('-__v -password')
@@ -26,8 +40,9 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
         const user = await User.create(args);
+        const token = signToken(user);
 
-        return user;
+        return{ token, user };
     },
     login: async (parent, { email, password }) => {
         const user = await User.findOne({ email });
@@ -41,8 +56,9 @@ const resolvers = {
         if (!correctPw) {
             throw new AuthenticationError('Incorrect credentials');
         }
-
-        return user;
+        
+        const token = signToken(user);
+        return { token, user };
     }
   }
 };
