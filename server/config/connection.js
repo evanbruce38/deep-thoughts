4a68e6a65ea44c602732578f5328762b28c4e8c6
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-11-02 16:52:16 -0400

    final script tweaks

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+mongoose.connect(
+  process.env.MONGODB_URI || 'https://glacial-inlet-73070.herokuapp.com/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
